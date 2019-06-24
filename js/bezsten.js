1f73a589a2bb3929825ad984c2aaf29c9ecf39cc
GarryRocksman <garryrocksmanjs@gmail.com>
2019-06-24 22:24:00 +0300

    added languages button, translate page to english

--- js/bezsten.js
+++ js/bezsten.js
@@ -1,3 +1,5 @@
+
+
 function openMenu() {
   $('.m-menu').addClass('m-menu__active');
 }
@@ -6,6 +8,49 @@ function closeMenu() {
   $('.m-menu').removeClass('m-menu__active');
 }
 
+function englishLanguage() {
+$('#en-contacts').css('display','flex');
+$('#en-podcast').css('display','flex');
+$('#en-missioners').css('display','flex');
+$('#en-services').css('display','flex');
+$('#en-webelieve').css('display','flex');
+$('#en-mission').css('display','flex');
+$('#en-main').css('display','flex');
+$('#en-menu').css('display','flex');
+
+$('#ru-contacts').css('display','none');
+$('#ru-podcast').css('display','none');
+$('#ru-missioners').css('display','none');
+$('#ru-services').css('display','none');
+$('#ru-webelieve').css('display','none');
+$('#ru-mission').css('display','none');
+$('#ru-main').css('display','none');
+$('#ru-menu').css('display','none');
+}
+
+
+function russianLanguage() {
+$('#ru-contacts').css('display','flex');
+$('#ru-podcast').css('display','flex');
+$('#ru-missioners').css('display','flex');
+$('#ru-services').css('display','flex');
+$('#ru-webelieve').css('display','flex');
+$('#ru-mission').css('display','flex');
+$('#ru-main').css('display','flex');
+$('#ru-menu').css('display','flex');
+
+$('#en-contacts').css('display','none');
+$('#en-podcast').css('display','none');
+$('#en-missioners').css('display','none');
+$('#en-services').css('display','none');
+$('#en-webelieve').css('display','none');
+$('#en-mission').css('display','none');
+$('#en-main').css('display','none');
+$('#en-menu').css('display','none');
+}
+
+
+
 
 $(document).scroll(function () {
   var y = $(this).scrollTop();
@@ -13,6 +58,8 @@ $(document).scroll(function () {
     $('.menu-background').fadeIn();
     $('.menu-link').css('color', '#1f2c30');
     $('.menu').css('color', '#1f2c30');
+    $('.languages').css('color', '#1f2c30');
+    $('.languages').css('border', '#1f2c30 solid 1px');
     $('#fa-bars').css('color', '#1f2c30');
   } else {
 
@@ -20,6 +67,8 @@ $(document).scroll(function () {
     $('.menu-background').fadeOut();
     $('.menu-link').css('color', 'white');
     $('.menu').css('color', 'white');
+    $('.languages').css('color', 'white');
+    $('.languages').css('border', 'white solid 1px');
   }
 
   if (y > 600) {
@@ -28,6 +77,7 @@ $(document).scroll(function () {
     $('.arrow-up').fadeOut();
   }
 });
+
 
 
 $(document).ready(function(){
