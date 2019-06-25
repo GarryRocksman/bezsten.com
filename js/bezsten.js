496a8879ca4e1742ccbee148b44650ece9b49e76
GarryRocksman <garryrocksmanjs@gmail.com>
2019-06-25 18:55:44 +0300

    added some annotations for functions

--- js/bezsten.js
+++ js/bezsten.js
@@ -1,38 +1,39 @@
 checkMenu();
-getLanguage();
+checkLanguage();
 
 
 
 
 
-
-function getLanguage() {
+// this function checking for laguage that was on the page before reloading
+function checkLanguage() {
   let lang = localStorage.getItem('language');
   if (lang !== null && lang === 'english') {
     englishLanguage();
   } else if (lang !== null && lang === 'russian') {
     russianLanguage();
   }
-
 }
 
-
-
-
+// spike!!!!
+// this function checking for position that menu was before reload and 
+// animate for adding menu-background color becouse it`s changing after scroll
 function checkMenu() {
   $('body,html').animate({
     scrollTop: 1
   }, 0.1);
 }
 
+//function for open mobile menu
 function openMenu() {
   $('.m-menu').addClass('m-menu__active');
 }
-
+//function for close mobile menu
 function closeMenu() {
   $('.m-menu').removeClass('m-menu__active');
 }
 
+//function for setting english language
 function englishLanguage() {
   localStorage.setItem('language', 'english');
   $('#en-contacts').css('display', 'flex');
@@ -57,6 +58,7 @@ function englishLanguage() {
 }
 
 
+//function for setting russian language
 function russianLanguage() {
   localStorage.setItem('language', 'russian');
   $('#ru-contacts').css('display', 'flex');
@@ -82,6 +84,7 @@ function russianLanguage() {
 
 
 
+//function for changing visualisation of menu bar and back-up arrow after scroll
 
 $(document).scroll(function () {
   var y = $(this).scrollTop();
@@ -110,6 +113,8 @@ $(document).scroll(function () {
 });
 
 
+
+//function for smooth page scrolling
 
 $(document).ready(function () {
   $(".menu").on("click", "a", function (event) {
